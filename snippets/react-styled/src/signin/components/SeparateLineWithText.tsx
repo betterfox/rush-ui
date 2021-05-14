5c1dd8713f908bf7e2b1693ace8d7239f4d4e6fb
tui2tone <tui2tone@gmail.com>
2021-05-15 04:23:01 +0700

    :sparkles: add error message display in signin form react styled

--- snippets/react-styled/src/signin/components/SeparateLineWithText.tsx
+++ snippets/react-styled/src/signin/components/SeparateLineWithText.tsx
@@ -15,7 +15,7 @@ const SeparateLineWithText = ({ text }: SeparateLineWithTextProps) => {
 };
 
 const Container = styled.div`${tw`relative block mb-8 w-40 mx-auto`}`;
-const HR = styled.hr`${tw`border border-background`}`;
+const HR = styled.hr`${tw`border border-outline`}`;
 const Text = styled.div`${tw`absolute top-1/2 left-1/2 bg-surface px-4 text-sm text-secondary transform -translate-x-2/4 -translate-y-2/4`}`;
 
 export default SeparateLineWithText;
